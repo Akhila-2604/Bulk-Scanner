78939736f777fcaca3c06979acc8f06cdba0a834
Akhila-2604 <110414533+Akhila-2604@users.noreply.github.com>
2026-06-26 11:45:48 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      // Proxies local calls to AbuseIPDB securely
-      '/api-abuse': {
-        target: 'https://api.abuseipdb.com/api/v2',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api-abuse/, ''),
-      },
-      // Proxies local calls to VirusTotal securely
-      '/api-vt': {
-        target: 'https://www.virustotal.com/api/v3',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api-vt/, ''),
-      },
-    },
-  },
 })
